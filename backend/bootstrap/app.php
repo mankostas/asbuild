6ca310124fa7874170a40b37fac09fc33a2b54ee
Kostas Anastasiou <anastasiou.ks@gmail.com>
2025-08-25 12:13:21 +0300

    Setup Corrections

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -4,7 +4,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+return Application::configure(
+    basePath: dirname(__DIR__),
+)
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
@@ -25,4 +27,5 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })->create()
+    ->usePublicPath(dirname(__DIR__, 2).'/public');
