5541827dd47cd29ff433fe5e5c577070e05b8cce
Kostas Anastasiou <50968774+mankostas@users.noreply.github.com>
2025-08-25 12:30:03 +0300

    Seed super admin user

--- backend/database/seeders/SuperAdminSeeder.php
+++ backend/database/seeders/SuperAdminSeeder.php
@@ -6,13 +6,13 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
-class UserSeeder extends Seeder
+class SuperAdminSeeder extends Seeder
 {
     public function run(): void
     {
         DB::table('users')->insert([
             'id' => 1,
-            'name' => 'Admin',
+            'name' => 'Super Admin',
             'email' => 'admin@example.com',
             'password' => Hash::make('password'),
             'tenant_id' => 1,
