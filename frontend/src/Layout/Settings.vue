2224d61f3cb7748cd2ec74b6e84daf8669da99a1
Kostas Anastasiou <50968774+mankostas@users.noreply.github.com>
2025-08-26 16:14:25 +0300

    feat: add theme settings store

--- frontend/src/Layout/Settings.vue
+++ frontend/src/Layout/Settings.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="fixed right-0 top-1/2 -translate-y-1/2 bg-white dark:bg-slate-800 shadow p-4">
-    Settings panel
-  </div>
+  <Settings />
 </template>
+<script setup>
+import Settings from '@/components/ui/Settings/index.vue';
+</script>
