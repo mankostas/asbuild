bffdb0567a82c9fde46632be6f3ab5262731a736
Kostas Anastasiou <50968774+mankostas@users.noreply.github.com>
2025-08-26 15:32:15 +0300

    fix: resolve vue imports

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
+    extensions: ['.mjs', '.js', '.mts', '.ts', '.jsx', '.tsx', '.json', '.vue'],
   },
   build: {
     outDir: '../public',
