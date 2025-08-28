1137a9cf525167aa799b0fab60b418b179f65abc
Kostas Anastasiou <50968774+mankostas@users.noreply.github.com>
2025-08-28 21:51:25 +0300

    chore: backfill tenant features and roles

---
+++ backend/database/seeders/TenantRolesBackfillSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Tenant;
+use Illuminate\Database\Seeder;
+
+class TenantRolesBackfillSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $defaultFeatures = ['appointments','roles','types','teams','statuses'];
+
+        Tenant::query()->lazy()->each(function (Tenant $tenant) use ($defaultFeatures) {
+            if (empty($tenant->features)) {
+                $tenant->features = $defaultFeatures;
+                $tenant->save();
+            }
+
+            DefaultFeatureRolesSeeder::syncDefaultRolesForFeatures($tenant);
+        });
+    }
+}
