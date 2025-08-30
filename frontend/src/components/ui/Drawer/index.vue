ffd601b3a514be802245e66cc6ec035e41ca0d2d
Kostas Anastasiou <50968774+mankostas@users.noreply.github.com>
2025-08-30 18:22:38 +0300

    feat: add templates drawer component

---
+++ frontend/src/components/ui/Drawer/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <TransitionRoot :show="open" as="template">
+    <Dialog as="div" class="relative z-[99999]" @close="$emit('close')">
+      <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0" enter-to="opacity-100" leave="duration-200 ease-in" leave-from="opacity-100" leave-to="opacity-0">
+        <div class="fixed inset-0 bg-slate-900/50" />
+      </TransitionChild>
+      <div class="fixed inset-0 flex justify-end">
+        <TransitionChild
+          as="template"
+          enter="transform transition ease-in-out duration-300"
+          enter-from="translate-x-full"
+          enter-to="translate-x-0"
+          leave="transform transition ease-in-out duration-300"
+          leave-from="translate-x-0"
+          leave-to="translate-x-full"
+        >
+          <DialogPanel class="w-full max-w-md h-full bg-white dark:bg-slate-800 shadow-xl rounded-l-2xl overflow-y-auto">
+            <slot />
+          </DialogPanel>
+        </TransitionChild>
+      </div>
+    </Dialog>
+  </TransitionRoot>
+</template>
+
+<script setup lang="ts">
+import { TransitionRoot, TransitionChild, Dialog, DialogPanel } from '@headlessui/vue';
+
+interface Props {
+  open: boolean;
+}
+
+defineProps<Props>();
+defineEmits(['close']);
+</script>
