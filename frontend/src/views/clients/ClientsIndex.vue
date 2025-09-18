b8172dd30afad28e33434865d598e35422831042
Kostas Anastasiou <50968774+mankostas@users.noreply.github.com>
2025-09-18 23:34:55 +0300

    Add modal routing for clients list

---
+++ frontend/src/views/clients/ClientsIndex.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="relative">
+    <ClientsList />
+    <RouterView #default="{ Component, route }">
+      <component :is="Component" v-if="Component && route?.meta?.modal" />
+    </RouterView>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import ClientsList from './ClientsList.vue';
+</script>
